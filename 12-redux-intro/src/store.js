e3e9446cf658692cb59de64435907da2db4f8435
clement dethoor <clement.dethoor@yahoo.fr>
2024-08-29 11:43:28 +0200

    End section 20 with redux

---
+++ 12-redux-intro/src/store.js
@@ -0,0 +1,25 @@
+// import { combineReducers, createStore } from "redux";
+// import { applyMiddleware } from "redux";
+// import { composeWithDevTools } from "@redux-devtools/extension";
+// import { thunk } from "redux-thunk";
+
+import accountReducer from "./features/accounts/accountSlice";
+import customerReducer from "./features/customers/customerSlice";
+
+import { configureStore } from "@reduxjs/toolkit";
+
+// const rootReducer = combineReducers({
+//   account: accountReducer,
+//   customer: customerReducer,
+// });
+
+// const store = createStore(
+//   rootReducer,
+//   composeWithDevTools(applyMiddleware(thunk))
+// );
+
+const store = configureStore({
+  reducer: { account: accountReducer, customer: customerReducer },
+});
+
+export default store;
--- readMe.md
+++ readMe.md
@@ -12,3 +12,9 @@
 
 Add this code to package.json `"server": "json-server --watch data/cities.json --port 9000 --delay 500"`
 Lunch the server :`npm run server`
+
+## To create project
+
+# With Vite.JS
+
+1- ``npm create vite@4```
